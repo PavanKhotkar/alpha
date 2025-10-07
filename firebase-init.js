6a8f3a9c91581ef9e95f8820f7299839cf4f9319
Pavan Khotkar <khotkarpavan3@gmail.com>
2025-10-07 12:49:22 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,16 +1,17 @@
+
 // ✅ Import Firebase modules directly from the CDN (browser-friendly)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 // ✅ Use your exact Firebase configuration (from your console)
 const firebaseConfig = {
-    apiKey: "AIzaSyC_eUNac7NmbhZjjGBni-vhb1IiPeU-tFM",
-  authDomain: "learned-trilogy-470903-r4.firebaseapp.com",
-  projectId: "learned-trilogy-470903-r4",
-  storageBucket: "learned-trilogy-470903-r4.firebasestorage.app",
-  messagingSenderId: "764207091261",
-  appId: "1:764207091261:web:9290672f78910e1afce454",
-  measurementId: "G-RLFXHE8XEJ"
+//    apiKey: "AIzaSyC_eUNac7NmbhZjjGBni-vhb1IiPeU-tFM",
+ // authDomain: "learned-trilogy-470903-r4.firebaseapp.com",
+ // projectId: "learned-trilogy-470903-r4",
+ // storageBucket: "learned-trilogy-470903-r4.firebasestorage.app",
+ // messagingSenderId: "764207091261",
+ // appId: "1:764207091261:web:9290672f78910e1afce454",
+ // measurementId: "G-RLFXHE8XEJ"
 };
 
 // ✅ Initialize Firebase and Firestore
@@ -37,5 +38,6 @@ async function readUserData() {
 
 addUserData();
 readUserData();
+
 
 
